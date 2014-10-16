269ad68267a272beee2bf236a0bb00b5725c5112
Rahul Malik <rahul.send89@gmail.com>
2014-10-16 19:27:06 +0530

    xmlParser init

--- jsobject.js
+++ jsobject.js
@@ -259,6 +259,26 @@ jsObject.prototype.html = function(html) {
     }
 };
 
+jsObject.parseXML = function( data ) {
+    var xml, tmp;
+    if ( !data || typeof data !== "string" ) {
+        return null;
+    }
+
+    // Support: IE9
+    try {
+        tmp = new DOMParser();
+        xml = tmp.parseFromString( data, "text/xml" );
+    } catch ( e ) {
+        xml = undefined;
+    }
+
+    if ( !xml || xml.getElementsByTagName( "parsererror" ).length ) {
+        jQuery.error( "Invalid XML: " + data );
+    }
+    return xml;
+};
+
 /*** Helper Functions ***/
 function toCamelCase(str) {
     return str.replace(/-([a-z])/ig, function( all, letter ) {
