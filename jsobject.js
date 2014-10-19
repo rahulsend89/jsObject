6f3ea2c436b277a48ced95f8f67aa6dd1ed76e86
Rahul Malik <rahul.send89@gmail.com>
2014-10-19 10:22:42 +0530

    Added Pause

--- jsobject.js
+++ jsobject.js
@@ -27,12 +27,27 @@ jsObject.prototype.removeEvent = function(evt, fn) {
 jsObject['startDeltaValue'] = 0;
 jsObject['endDeltaValue'] = 100;
 jsObject['animationTimer'] = [];
+jsObject.isPlaying = false;
 jsObject.pause = function() {
-    clearInterval(this.animationtimer);
+    if (this.isPlaying) {
+        this.isPlaying = false;
+        var len = this.animationtimer.length;
+        for (var i = 0; i < this.animationtimer.length; i++) {
+            clearInterval(this.animationtimer[i]);
+        };
+    }else{
+        this.isPlaying = true;
+        var len = this.animationtimer.length;
+        for (var i = 0; i < this.animationtimer.length; i++) {
+            this.animationtimer[i] = setInterval(this.callFunction[i],1);
+        };
+    }
     return this;
 };
+
 jsObject.prototype.pause = function() {
     jsObject.pause();
+    return this;
 };
 jsObject.animate = function(el, obj) {
     if (!obj) {
@@ -45,7 +60,7 @@ jsObject.animate = function(el, obj) {
             propNum++;
             var strVal = toCamelCase(styleValue);
             var eleStyle = el.style;
-            var easeVal = (!obj['ease'])?"linearTween":obj['ease'];
+            var easeVal = (!obj['ease']) ? "linearTween" : obj['ease'];
             if (!eleStyle.hasOwnProperty(strVal)) {
                 if (styleValue == 'time') {
                     this.endDeltaValue = obj[styleValue];
@@ -54,6 +69,11 @@ jsObject.animate = function(el, obj) {
                     easeVal = obj['ease'];
                 }
             }
+            if (!this.animationtimer) {
+                this.animationtimer = [];
+                this.callFunction = [];
+            }
+            this.animationtimer[propNum] = 0;
             var startVal = el.style[styleValue];
             startVal = (!startVal) ? 0 : parseInt(startVal);
             this.startDeltaValue = 0;
@@ -63,17 +83,21 @@ jsObject.animate = function(el, obj) {
 }
 jsObject.makeThisPropertyAnimate = function(el, startVal, styleValue, endVal, propNum, ease) {
     var that = this;
+    this.isPlaying = true;
     var styleChange = el.style;
     var animationTimer = (function() {
-        return setInterval(function() {
+        that.callFunction[propNum] = function() {
             if (that.startDeltaValue <= that.endDeltaValue) {
                 that.startDeltaValue++;
                 var calVal = that[ease](that.startDeltaValue, startVal, endVal - startVal, that.endDeltaValue) + "px";
                 styleChange[styleValue] = calVal;
             } else {
                 clearInterval(animationTimer);
+                this.isPlaying = false;
             }
-        }, 1)
+        }
+        that.animationtimer[propNum] = setInterval(that.callFunction[propNum], 1)
+        return that.animationtimer[propNum];
     })();
 };
 jsObject.prototype.animate = function(obj) {
