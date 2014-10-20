999a292b7e00b5e9a3188adcdea235eec3dca3b6
Rahul Malik <rahul.send89@gmail.com>
2014-10-20 17:25:00 +0530

    Added file just for using animation

--- jsobject.js
+++ jsobject.js
@@ -1,17 +1,3 @@
-function jsObject(obj) {
-    if (this === window) {
-        return new jsObject(obj);
-    }
-    var type = typeof obj;
-    if (type === "string") {
-        this.el = document.getElementById(obj);
-    } else if (type === "object" && obj.nodeType !== "undefined" && obj.nodeType === 1) {
-        this.el = obj;
-    } else {
-        throw new Error("Argument is of wrong type");
-    }
-}
-
 /*** Event instance methods   ***/
 jsObject.prototype.addEvent = function(evt, fn) {
     jsObject.addEvent(this.el, evt, fn);
@@ -22,86 +8,6 @@ jsObject.prototype.addEvent = function(evt, fn) {
 jsObject.prototype.removeEvent = function(evt, fn) {
     jsObject.removeEvent(this.el, evt, fn);
 
-    return this;
-};
-jsObject['startDeltaValue'] = 0;
-jsObject['endDeltaValue'] = 100;
-jsObject['animationTimer'] = [];
-jsObject.isPlaying = false;
-jsObject.pause = function() {
-    if (this.isPlaying) {
-        this.isPlaying = false;
-        var len = this.animationtimer.length;
-        for (var i = 0; i < this.animationtimer.length; i++) {
-            clearInterval(this.animationtimer[i]);
-        };
-    }else{
-        this.isPlaying = true;
-        var len = this.animationtimer.length;
-        for (var i = 0; i < this.animationtimer.length; i++) {
-            this.animationtimer[i] = setInterval(this.callFunction[i],1);
-        };
-    }
-    return this;
-};
-
-jsObject.prototype.pause = function() {
-    jsObject.pause();
-    return this;
-};
-jsObject.animate = function(el, obj) {
-    if (!obj) {
-        throw {
-            message: "Invalid argument"
-        };
-    } else {
-        var propNum = 0
-        for (styleValue in obj) {
-            propNum++;
-            var strVal = toCamelCase(styleValue);
-            var eleStyle = el.style;
-            var easeVal = (!obj['ease']) ? "linearTween" : obj['ease'];
-            if (!eleStyle.hasOwnProperty(strVal)) {
-                if (styleValue == 'time') {
-                    this.endDeltaValue = obj[styleValue];
-                }
-                if (styleValue == 'ease') {
-                    easeVal = obj['ease'];
-                }
-            }
-            if (!this.animationtimer) {
-                this.animationtimer = [];
-                this.callFunction = [];
-            }
-            this.animationtimer[propNum] = 0;
-            var startVal = el.style[styleValue];
-            startVal = (!startVal) ? 0 : parseInt(startVal);
-            this.startDeltaValue = 0;
-            this.makeThisPropertyAnimate(el, startVal, styleValue, obj[styleValue], propNum, easeVal);
-        }
-    }
-}
-jsObject.makeThisPropertyAnimate = function(el, startVal, styleValue, endVal, propNum, ease) {
-    var that = this;
-    this.isPlaying = true;
-    var styleChange = el.style;
-    var animationTimer = (function() {
-        that.callFunction[propNum] = function() {
-            if (that.startDeltaValue <= that.endDeltaValue) {
-                that.startDeltaValue++;
-                var calVal = that[ease](that.startDeltaValue, startVal, endVal - startVal, that.endDeltaValue) + "px";
-                styleChange[styleValue] = calVal;
-            } else {
-                clearInterval(animationTimer);
-                this.isPlaying = false;
-            }
-        }
-        that.animationtimer[propNum] = setInterval(that.callFunction[propNum], 1)
-        return that.animationtimer[propNum];
-    })();
-};
-jsObject.prototype.animate = function(obj) {
-    jsObject.animate(this.el, obj);
     return this;
 };
 var arForFun = ['click', 'mouseover', 'mouseout'];
@@ -385,9 +291,6 @@ var getStyle = (function() {
 log = function(val) {
     console.log(val);
 }
-jsObject.linearTween = function(t, b, c, d) {
-    return c * t / d + b;
-};
 /*** Language Extensions ***/
 if (typeof String.prototype.trim === "undefined") {
     String.prototype.trim = function() {
