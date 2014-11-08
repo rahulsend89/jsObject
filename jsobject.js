13d58e1280db9c7272f15157068427ccd6fd9ae7
Rahul Malik <rahul.send89@gmail.com>
2014-11-08 21:38:21 +0530

    Update backup 8 Nov

--- jsobject.js
+++ jsobject.js
@@ -1,29 +1,42 @@
-/*** Event instance methods   ***/
-jsObject.prototype.addEvent = function(evt, fn) {
-    jsObject.addEvent(this.el, evt, fn);
-
-    return this;
-};
-
-jsObject.prototype.removeEvent = function(evt, fn) {
-    jsObject.removeEvent(this.el, evt, fn);
-
-    return this;
-};
-var arForFun = ['click', 'mouseover', 'mouseout'];
-var len = arForFun.length;
-for (var inc = 0; inc < len; inc++) {
-    var tempVal = arForFun[inc];
-    jsObject.prototype[tempVal] = (function(arVar) {
-        return function(fn) {
-            var that = this;
-            jsObject.addEvent(this.el, arVar, function(e) {
-                fn.call(that, e);
-            });
-            return this;
-        };
-    })(arForFun[inc]);
+log = function(val) {
+    console.log(val);
 }
+/**
+ * Making Prototype of ar[string] using loop
+ */
+var arForFunClass = ["addClass", "removeClass", "toggleClass", "hasClass", "css", "click", "mouseover", "mouseout", "addEvent", "removeEvent"];
+for (var inc = 0; inc < arForFunClass.length; inc++) {
+    var tempVal = arForFunClass[inc];
+    jsObject.prototype[tempVal] = (function(arVar) {
+        if (arVar == "css") {
+            return function(css, value) {
+                jsObject[arVar](this.el, css, value);
+            }
+        } else if (arVar == "click" || arVar == "mouseover" || arVar == "mouseout") {
+            return function(fn) {
+                var that = this;
+                jsObject.addEvent(this.el, arVar, function(e) {
+                    fn.call(that, e);
+                });
+                return this;
+            };
+
+        }else if(arVar=="addEvent"||arVar=="removeEvent"){
+            return function(evt,fn) {
+                var that = this;
+                jsObject[arVar](this.el, evt, function(e) {
+                    fn.call(that, e);
+                });
+                return this;
+            };
+        } else {
+            return function(classVal) {
+                jsObject[arVar](this.el, classVal);
+            }
+        }
+    })(tempVal);
+}
+
 /*** Event static methods ***/
 if (typeof addEventListener !== "undefined") {
     jsObject.addEvent = function(obj, evt, fn) {
@@ -81,149 +94,141 @@ if (typeof addEventListener !== "undefined") {
 }
 
 /*** Style static methods ***/
-jsObject.css = function(el, css, value) {
-    var cssType = typeof css,
-        valueType = typeof value,
-        elStyle = el.style;
+jsObject.extend({
+    css: function(el, css, value) {
+        var cssType = typeof css,
+            valueType = typeof value,
+            elStyle = el.style;
 
-    if (cssType !== "undefined" && valueType === "undefined") {
-        if (cssType === "object") {
-            // set style info
-            for (var prop in css) {
-                if (css.hasOwnProperty(prop)) {
-                    elStyle[toCamelCase(prop)] = css[prop];
+        if (cssType !== "undefined" && valueType === "undefined") {
+            if (cssType === "object") {
+                // set style info
+                for (var prop in css) {
+                    if (css.hasOwnProperty(prop)) {
+                        elStyle[toCamelCase(prop)] = css[prop];
+                    }
                 }
+            } else if (cssType === "string") {
+                // get style info for specified property
+                return getStyle(el, css);
+            } else {
+                throw {
+                    message: "Invalid parameter passed to css()"
+                };
             }
-        } else if (cssType === "string") {
-            // get style info for specified property
-            return getStyle(el, css);
+
+        } else if (cssType === "string" && valueType === "string") {
+            elStyle[toCamelCase(css)] = value;
+
         } else {
             throw {
-                message: "Invalid parameter passed to css()"
+                message: "Invalid parameters passed to css()"
+            };
+        }
+    },
+    hasClass: function(el, value) {
+        return (" " + el.className + " ").indexOf(" " + value + " ") > -1;
+    },
+    addClass: function(el, value) {
+        var className = el.className;
+
+        if (!className) {
+            el.className = value;
+        } else {
+            var classNames = value.split(/\s+/),
+                l = classNames.length;
+
+            for (var i = 0; i < l; i++) {
+                if (!this.hasClass(el, classNames[i])) {
+                    className += " " + classNames[i];
+                }
+            }
+
+            el.className = className.trim();
+        }
+    },
+    removeClass: function(el, value) {
+        if (value) {
+            var classNames = value.split(/\s+/),
+                className = " " + el.className + " ",
+                l = classNames.length;
+
+            for (var i = 0; i < l; i++) {
+                className = className.replace(" " + classNames[i] + " ", " ");
+            }
+
+            el.className = className.trim();
+
+        } else {
+            el.className = "";
+        }
+    },
+    toggleClass: function(el, value) {
+        var classNames = value.split(/\s+/),
+            i = 0,
+            className;
+
+        while (className = classNames[i++]) {
+            this[this.hasClass(el, className) ? "removeClass" : "addClass"](el, className);
+        }
+    },
+    createElement: function(obj) {
+        if (!obj || !obj.tagName) {
+            throw {
+                message: "Invalid argument"
             };
         }
 
-    } else if (cssType === "string" && valueType === "string") {
-        elStyle[toCamelCase(css)] = value;
+        var el = document.createElement(obj.tagName);
+        obj.id && (el.id = obj.id);
+        obj.className && (el.className = obj.className);
+        obj.html && (el.innerHTML = obj.html);
 
-    } else {
-        throw {
-            message: "Invalid parameters passed to css()"
-        };
-    }
-};
+        if (typeof obj.attributes !== "undefined") {
+            var attr = obj.attributes,
+                prop;
 
-jsObject.hasClass = function(el, value) {
-    return (" " + el.className + " ").indexOf(" " + value + " ") > -1;
-};
-
-jsObject.addClass = function(el, value) {
-    var className = el.className;
-
-    if (!className) {
-        el.className = value;
-    } else {
-        var classNames = value.split(/\s+/),
-            l = classNames.length;
-
-        for (var i = 0; i < l; i++) {
-            if (!this.hasClass(el, classNames[i])) {
-                className += " " + classNames[i];
+            for (prop in attr) {
+                if (attr.hasOwnProperty(prop)) {
+                    el.setAttribute(prop, attr[prop]);
+                }
             }
         }
 
-        el.className = className.trim();
-    }
-};
+        if (typeof obj.children !== "undefined") {
+            var child,
+                i = 0;
 
-jsObject.removeClass = function(el, value) {
-    if (value) {
-        var classNames = value.split(/\s+/),
-            className = " " + el.className + " ",
-            l = classNames.length;
-
-        for (var i = 0; i < l; i++) {
-            className = className.replace(" " + classNames[i] + " ", " ");
+            while (child = obj.children[i++]) {
+                el.appendChild(this.createElement(child));
+            }
         }
-
-        el.className = className.trim();
-
-    } else {
-        el.className = "";
+        return el;
     }
-};
+});
 
-jsObject.toggleClass = function(el, value) {
-    var classNames = value.split(/\s+/),
-        i = 0,
-        className;
 
-    while (className = classNames[i++]) {
-        this[this.hasClass(el, className) ? "removeClass" : "addClass"](el, className);
-    }
-};
-
-/*** Style instance methods ***/
-jsObject.prototype.css = function(css, value) {
-    return jsObject.css(this.el, css, value) || this;
-};
-
-jsObject.prototype.addClass = function(value) {
-    jsObject.addClass(this.el, value);
-
-    return this;
-};
-
-jsObject.prototype.removeClass = function(value) {
-    jsObject.removeClass(this.el, value);
-
-    return this;
-};
-
-jsObject.prototype.toggleClass = function(value) {
-    jsObject.toggleClass(this.el, value);
-
-    return this;
-};
-
-jsObject.prototype.hasClass = function(value) {
-    return jsObject.hasClass(this.el, value);
-};
 
 /*** DOM Object Stuff ***/
-jsObject.createElement = function(obj) {
-    if (!obj || !obj.tagName) {
-        throw {
-            message: "Invalid argument"
-        };
+
+jsObject.parseXML = function(data) {
+    var xml, tmp;
+    if (!data || typeof data !== "string") {
+        return null;
     }
 
-    var el = document.createElement(obj.tagName);
-    obj.id && (el.id = obj.id);
-    obj.className && (el.className = obj.className);
-    obj.html && (el.innerHTML = obj.html);
-
-    if (typeof obj.attributes !== "undefined") {
-        var attr = obj.attributes,
-            prop;
-
-        for (prop in attr) {
-            if (attr.hasOwnProperty(prop)) {
-                el.setAttribute(prop, attr[prop]);
-            }
-        }
+    // Support: IE9
+    try {
+        tmp = new DOMParser();
+        xml = tmp.parseFromString(data, "text/xml");
+    } catch (e) {
+        xml = undefined;
     }
 
-    if (typeof obj.children !== "undefined") {
-        var child,
-            i = 0;
-
-        while (child = obj.children[i++]) {
-            el.appendChild(this.createElement(child));
-        }
+    if (!xml || xml.getElementsByTagName("parsererror").length) {
+        jQuery.error("Invalid XML: " + data);
     }
-
-    return el;
+    return xml;
 };
 
 jsObject.prototype.append = function(data) {
@@ -250,26 +255,6 @@ jsObject.prototype.html = function(html) {
     }
 };
 
-jsObject.parseXML = function(data) {
-    var xml, tmp;
-    if (!data || typeof data !== "string") {
-        return null;
-    }
-
-    // Support: IE9
-    try {
-        tmp = new DOMParser();
-        xml = tmp.parseFromString(data, "text/xml");
-    } catch (e) {
-        xml = undefined;
-    }
-
-    if (!xml || xml.getElementsByTagName("parsererror").length) {
-        jQuery.error("Invalid XML: " + data);
-    }
-    return xml;
-};
-
 /*** Helper Functions ***/
 function toCamelCase(str) {
     return str.replace(/-([a-z])/ig, function(all, letter) {
@@ -288,9 +273,6 @@ var getStyle = (function() {
         };
     }
 }());
-log = function(val) {
-    console.log(val);
-}
 /*** Language Extensions ***/
 if (typeof String.prototype.trim === "undefined") {
     String.prototype.trim = function() {
