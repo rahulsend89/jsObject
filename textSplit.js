5bac8c6cdac9c0e485a80d5268ff5852182808e8
Rahul Malik <rahul.send89@gmail.com>
2014-11-28 22:28:53 +0530

    used variable

--- textSplit.js
+++ textSplit.js
@@ -1,13 +1,15 @@
 jsObject.extend({
 	expand:function(el){
 		var allchar = el.innerHTML.split(" ");		
-		var str = "<div style='position:relative;display:inline;'>"+allchar.join("</div> <div style='position:relative;display:inline;'>")+"</div>";
+		var startdiv = "<div style='position:relative;display:inline;'>";
+		var enddiv = "</div>";
+		var str = startdiv+""+allchar.join(enddiv+" "+startdiv)+""+enddiv;
 		el.innerHTML = str;		
 		var words = el.children;
 		var all_char = [];
 		for (var i = 0 ; i < el.children.length; i++) {
 			var _char = el.children[i];			
-			_char.innerHTML = "<div style='position:relative;display:inline;'>"+_char.innerHTML.split("").join("</div><div style='position:relative;display:inline;'>")+"</div>";
+			_char.innerHTML = startdiv+""+_char.innerHTML.split("").join(enddiv+""+startdiv)+""+enddiv;
 			for (var j = 0; j < _char.children.length; j++) {
 				all_char.push(_char.children[j]);
 			};			
