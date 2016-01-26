3088de007677b3b3c37d98dc75fc062c17101530
Rahul Malik <rahul.send89@gmail.com>
2016-01-26 18:49:45 +0530

    test(testjsObject): added missing test for passing travis build

---
+++ src/jsObjectAnimation.Spec.js
@@ -0,0 +1,8 @@
+describe("when song has been paused", function() {
+    beforeEach(function() {
+
+    });
+    it("should indicate that the song is currently paused", function() {
+        expect(jsObject).toBeTruthy();
+    });
+});
