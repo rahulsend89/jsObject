f9337442a0a74b89d5223485ba74f41a0935ad9a
Rahul Malik <rahul.send89@gmail.com>
2016-01-26 18:57:09 +0530

    chore(remove):

--- my.conf.js
+++ my.conf.js
@@ -54,7 +54,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox' , 'Chrome' , 'PhantomJS'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
