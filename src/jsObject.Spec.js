6abb1badf88a79bc63ebbfcb4913f016d9dc66c0
Rahul Malik <rahul.send89@gmail.com>
2016-01-31 20:43:47 +0530

    fix(mouse event fix):

--- src/jsObject.Spec.js
+++ src/jsObject.Spec.js
@@ -10,13 +10,58 @@ describe("when jsObject is included", function() {
             _element.el.backgroundColor= "white";
             _element.html('<b id="test">Hello, World!</b><a><u>Text</u></a>');
         },
-        trigger = function(el, etype){
-            if (el.fireEvent) {
-                (el.fireEvent('on' + etype));
+        trigger = function(node, eventName) {
+            // Make sure we use the ownerDocument from the provided node to avoid cross-window problems
+            var doc;
+            if (node.ownerDocument) {
+                doc = node.ownerDocument;
+            } else if (node.nodeType == 9){
+                // the node may be the document itself, nodeType 9 = DOCUMENT_NODE
+                doc = node;
             } else {
-                var evObj = document.createEvent('Events');
-                evObj.initEvent(etype, true, false);
-                el.dispatchEvent(evObj);
+                throw new Error("Invalid node passed to fireEvent: " + node.id);
+            }
+
+            if (node.dispatchEvent) {
+                // Gecko-style approach (now the standard) takes more work
+                var eventClass = "";
+
+                // Different events have different event classes.
+                // If this switch statement can't map an eventName to an eventClass,
+                // the event firing is going to fail.
+                switch (eventName) {
+                    case "click": // Dispatching of 'click' appears to not work correctly in Safari. Use 'mousedown' or 'mouseup' instead.
+                    case "mousedown":
+                    case "mouseup":
+                    case "mouseover":
+                    case "mouseout":
+                        eventClass = "MouseEvents";
+                        break;
+
+                    case "focus":
+                    case "change":
+                    case "blur":
+                    case "select":
+                        eventClass = "HTMLEvents";
+                        break;
+
+                    default:
+                        throw "fireEvent: Couldn't find an event class for event '" + eventName + "'.";
+                        break;
+                }
+                var event = doc.createEvent(eventClass);
+
+                var bubbles = eventName == "change" ? false : true;
+                event.initEvent(eventName, bubbles, true); // All events created as bubbling and cancelable.
+
+                event.synthetic = true; // allow detection of synthetic events
+                // The second parameter says go ahead with the default action
+                node.dispatchEvent(event, true);
+            } else  if (node.fireEvent) {
+                // IE-old school style
+                var event = doc.createEventObject();
+                event.synthetic = true; // allow detection of synthetic events
+                node.fireEvent("on" + eventName, event);
             }
         };
     beforeAll(function() {
@@ -109,10 +154,10 @@ describe("when jsObject is included", function() {
     });
     it("addEvent should work", function () {
         var doneFn = jasmine.createSpy("addEvent");
-        jsObject.addEvent(document, "keydown", function(){
+        jsObject.addEvent(document, "focus", function(){
             doneFn();
         });
-        trigger(document,"keydown");
+        trigger(document,"focus");
         expect(doneFn).toHaveBeenCalled();
     });
     it("removeEvent should work", function () {
@@ -123,12 +168,12 @@ describe("when jsObject is included", function() {
                 addEventFn();
             };
 
-        jsObject.addEvent(document, "keydown", callBackFun);
-        trigger(document,"keydown");
+        jsObject.addEvent(document, "focus", callBackFun);
+        trigger(document,"focus");
         expect(addEventFn).toHaveBeenCalled();
-        jsObject.removeEvent(document,"keydown", callBackFun );
+        jsObject.removeEvent(document,"focus", callBackFun );
         removeEventFn = false
-        trigger(document,"keydown");
+        trigger(document,"focus");
         expect(removeEventFn).toBeFalsy();
     });
     it("objToParams should work", function () {
